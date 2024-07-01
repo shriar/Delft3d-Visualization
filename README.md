5b405b329076b9531f66f2dc5ec43fcef443576d
shahriar <mdshahriar412@gmail.com>
2024-07-01 10:28:45 +0600

    .nc and .obs file detection

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Plotting of delft3d-flow output file
 
+## Steps of setup:
 1. Add `FlNcdf = #hismap#` this line at the end of your `*.mdf` file
 2. set the monitoring/observation point name to `*.tek` file name. (e.g. AKYABIHO)
 3. install python
 4. open this folder in a terminal, run `pip install -r requirements.txt`
 5. add the `plot.py` inside you model folder
-5. in the `plot.py`, change the observation and histrory file name according to your need. (e.g. `*.obs`  and `trih-*.nc`)
 6. start the run of your model and after the model keeps running, run `plot.py`. You will see the changing plot.
 
-(note: `*` means anything)
 
-TODO
+## Assumptions:
+1. there is only one `trih-*.nc` file and one `*.obs` file in the model folder
+
+## TODO
 - [ ] support of csv as obserbation file 
 - [ ] changing of number of plot according to monitoring point
 
+(note: `*` means anything)
 ![img](img1.png)
--- plot.py
+++ plot.py
@@ -8,6 +8,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from sklearn.metrics import r2_score
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(description='description')
 parser.add_argument('--obs', type=str, default='coarser.obs', help='')
@@ -113,18 +114,19 @@ def plot(obs_station_name, model_interpolated, obs_interpolated, fig, axs):
     plt.draw()
 
 class Watcher(FileSystemEventHandler):
-    def __init__(self, args, fig, axs):
-        self.args = args
+    def __init__(self, nc_file, obs_file, fig, axs):
+        self.nc_file = nc_file
+        self.obs_file = obs_file
         self.fig = fig
         self.axs = axs
 
     def on_modified(self, event):
-        if event.src_path.endswith(self.args.nc):
-            update_plot(self.args, self.fig, self.axs)
+        if event.src_path.endswith(self.nc_file):
+            update_plot(self.nc_file, self.obs_file, self.fig, self.axs)
 
-def update_plot(args, fig, axs):
-    obs_station_name = extract_station_name(args.obs)
-    model_dataset = xr.open_dataset(args.nc)
+def update_plot(nc_file, obs_file, fig, axs):
+    obs_station_name = extract_station_name(obs_file)
+    model_dataset = xr.open_dataset(nc_file)
     obs_df = make_obs_df(obs_station_name)
     model_df = make_model_df(model_dataset, obs_station_name)
     model_df, obs_df = clipped_dataframe(model_df, obs_df)
@@ -132,13 +134,19 @@ def update_plot(args, fig, axs):
     plot(obs_station_name, model_interpolated, obs_interpolated, fig, axs)
 
 fig, axs = plt.subplots(2, 2, figsize=(12, 10))
-watcher = Watcher(args, fig, axs)
+nc_files = glob.glob('trih*.nc')
+obs_files = glob.glob('*.obs')
+if len(obs_files) == 1:
+    nc_file = nc_files[0]
+    obs_file = obs_files[0]
+
+watcher = Watcher(nc_file, obs_file, fig, axs)
 observer = Observer()
 observer.schedule(watcher, path='.', recursive=False)
 observer.start()
 
 # Initial plot before starting the observer
-update_plot(args, fig, axs)
+update_plot(nc_file, obs_file, fig, axs)
 
 try:
     plt.show()
